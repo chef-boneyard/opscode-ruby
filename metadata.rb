a928528ef20600dd7d79332ac67fda1235250966
Christopher Maier <cm@opscode.com>
2013-03-12 17:36:30 -0400

    Add some comments

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,7 @@ description      "Installs/Configures Ruby for Opscode's infrastructure"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+# This is a community cookbook:
+# http://community.opscode.com/cookbooks/rbenv
+# https://github.com/RiotGames/rbenv-cookbook
 depends "rbenv", "~> 1.4.1"
