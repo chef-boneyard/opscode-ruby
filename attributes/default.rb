ff2ddf52f3fc62dc71f6b48235c8d6e93f3c7e83
sersut <serdar@opscode.com>
2013-07-18 16:55:58 +0300

    Make ruby_versions, global ruby version and base gems configurable.

---
+++ attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: opscode-ruby
+# Attributes:: default
+#
+# Copyright 2013, Opscode
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['opscode-ruby']['versions'] = [ "1.9.3-p385" ]
+default['opscode-ruby']['base_gems'] = [ "bundler", "mixlib-shellout" ]
+default['opscode-ruby']['global'] = "1.9.3-p385"
