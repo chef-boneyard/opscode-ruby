1f2cb502ffa021a23411a322d2dd752f24baed49
Seth Chisamore <schisamo@opscode.com>
2013-10-31 17:12:44 -0400

    bump rbenv cookbook to 1.6.5

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ version          "1.0.0"
 # This is a community cookbook:
 # http://community.opscode.com/cookbooks/rbenv
 # https://github.com/RiotGames/rbenv-cookbook
-depends "rbenv", "~> 1.4.1"
+depends "rbenv", "~> 1.6.5"
 
 # This is required to install ruby on windows
 depends "7-zip", "~> 1.0.0"
